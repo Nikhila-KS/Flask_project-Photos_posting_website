2ebb63b17a395d272d7f2248f12ef0d3f5420ec0
Nikhila-KS <100426366+Nikhila-KS@users.noreply.github.com>
2022-07-30 01:34:50 +0530

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from os import path
+from flask_socketio import SocketIO
+
+db = SQLAlchemy()
+socketio = SocketIO()
+
+def createapp():
+    #Initializing the app
+    app = Flask(__name__)
+    app.config['SECRET_KEY']='hello'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    db.init_app(app)
+    loginmanager = LoginManager(app)
+    loginmanager.login_view = '/'
+    socketio.init_app(app)
+
+    #Making the routes
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import Users
+    createdatabase(app)
+
+    @loginmanager.user_loader
+    def userloader(id):
+        return Users.query.filter_by(id=int(id)).first()
+
+    #Returning the app
+    return socketio, app
+
+def createdatabase(app):
+    if not path.exists("website/database.db"):
+        db.create_all(app=app)
+        print('Created')
